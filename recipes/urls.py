7ca01db30d99c71825ef3432aaa53273506a1ff9
felipealmeidasilva <filipealmeidakukri@hotmail.com>
2024-11-19 21:14:36 -0300

    add new tasks

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from recipes.views import sobre, contato
+from recipes.views import sobre, contato, home
 
 
 urlpatterns = [
+    path("", home),
     path('sobre/', sobre),
     path('contato/', contato),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
+
+def home(request):
+    return render(request, 'recipes/home.html')
 def sobre(request):
-    return HttpResponse("Sobre")
+    return render(request, 'sobre.html')
 
 def contato(request):
     return HttpResponse("Contato")
